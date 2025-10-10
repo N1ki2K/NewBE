a4786ead2dd43ce88fbee6c8066172d6c39cdc01
Nikola Panayotov <pan07nik@gmail.com>
2025-10-10 16:29:10 +0300

    s

--- api/cors.php
+++ api/cors.php
@@ -11,7 +11,7 @@ function handleCORS() {
         header("Access-Control-Allow-Origin: $origin");
     } else {
         // Allow all origins in development, restrict in production
-        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Origin: https://nukgsz.com/");
     }
 
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH, OPTIONS");
