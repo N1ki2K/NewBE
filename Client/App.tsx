e89fc7f0d0f8bb0e7c0b764b3b4f464171a0502e
Nikola Panayotov <pan07nik@gmail.com>
2025-10-12 19:19:40 +0300

    asd

--- Client/App.tsx
+++ Client/App.tsx
@@ -13,25 +13,8 @@ import PatronPage from './pages/school/PatronPage';
 import TeamPage from './pages/school/TeamPage';
 import CouncilPage from './pages/school/CouncilPage';
 import SchoolNewsPage from './pages/school/SchoolNewsPage';
-import CalendarPage from './pages/documents/CalendarPage';
-import SchedulesPage from './pages/documents/SchedulesPage';
-import BudgetReportsPage from './pages/documents/BudgetReportsPage';
-import RulesPage from './pages/documents/RulesPage';
-import EthicsCodePage from './pages/documents/EthicsCodePage';
-import AdminServicesPage from './pages/documents/AdminServicesPage';
-import AdmissionsPage from './pages/documents/AdmissionsPage';
-import RoadSafetyPage from './pages/documents/RoadSafetyPage';
-import OresPage from './pages/documents/OresPage';
-import ContinuingEducationPage from './pages/documents/ContinuingEducationPage';
-import FaqPage from './pages/documents/FaqPage';
-import AnnouncementPage from './pages/documents/AnnouncementPage';
-import StudentsPage from './pages/documents/StudentsPage';
-import OlympiadsPage from './pages/documents/OlympiadsPage';
 import UsefulLinksPage from './pages/UsefulLinksPage';
 import GalleryPage from './pages/GalleryPage';
-import YourHourPage from './pages/projects/YourHourPage';
-import SupportSuccessPage from './pages/projects/SupportSuccessPage';
-import EducationTomorrowPage from './pages/projects/EducationTomorrowPage';
 import ContactsPage from './pages/ContactsPage';
 import InfoAccessPage from './pages/InfoAccessPage';
 import NotFoundPage from './pages/NotFoundPage';
@@ -41,7 +24,6 @@ import CMSDashboard from './components/cms/CMSDashboard';
 import NewsArticlePage from './pages/NewsArticlePage';
 import NewsPage from './pages/NewsPage';
 import EventsPage from './pages/EventsPage';
-import PDFViewerPage from './pages/documents/PDFViewerPage';
 import PDFDocumentPage from './pages/documents/PDFDocumentPage';
 import PresentationViewerPage from './pages/projects/PresentationViewerPage';
 import PresentationEmbedPage from './pages/projects/PresentationEmbedPage';
@@ -68,30 +50,14 @@ const App: React.FC = () => {
               <Route path="/school/news" element={<SchoolNewsPage />} />
 
               {/* Документи */}
-              <Route path="/documents/calendar" element={<CalendarPage />} />
-              <Route path="/documents/schedules" element={<SchedulesPage />} />
-              <Route path="/documents/budget" element={<BudgetReportsPage />} />
-              <Route path="/documents/rules" element={<RulesPage />} />
-              <Route path="/documents/ethics" element={<EthicsCodePage />} />
-              <Route path="/documents/admin-services" element={<AdminServicesPage />} />
-              <Route path="/documents/admissions" element={<AdmissionsPage />} />
-              <Route path="/documents/road-safety" element={<RoadSafetyPage />} />
-              <Route path="/documents/ores" element={<OresPage />} />
-              <Route path="/documents/continuing-education" element={<ContinuingEducationPage />} />
-              <Route path="/documents/faq" element={<FaqPage />} />
-              <Route path="/documents/announcement" element={<AnnouncementPage />} />
-              <Route path="/documents/students" element={<StudentsPage />} />
-              <Route path="/documents/olympiads" element={<OlympiadsPage />} />
-              <Route path="/documents/docs/*" element={<PDFViewerPage />} />
-              <Route path="/documents/pdf/:filename" element={<PDFViewerPage />} />
               <Route path="/documents/embed/:filename" element={<PDFDocumentPage />} />
+              <Route path="/documents/pdf/:filename" element={<PDFDocumentPage />} />
+              <Route path="/documents/*" element={<DynamicPage />} />
 
               {/* Работа по проекти */}
-              <Route path="/projects/your-hour" element={<YourHourPage />} />
-              <Route path="/projects/support-for-success" element={<SupportSuccessPage />} />
-              <Route path="/projects/education-for-tomorrow" element={<EducationTomorrowPage />} />
-              <Route path="/projects/presentations/view/:filename" element={<PresentationViewerPage />} />
               <Route path="/projects/presentations/embed/:filename" element={<PresentationEmbedPage />} />
+              <Route path="/projects/presentations/view/:filename" element={<PresentationViewerPage />} />
+              <Route path="/projects/*" element={<DynamicPage />} />
 
               {/* Other main links */}
               <Route path="/useful-links" element={<UsefulLinksPage />} />
