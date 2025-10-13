221d4eb1acdee7a68ae1d2dd6d5102fcdd144262
Nikola Panayotov <pan07nik@gmail.com>
2025-10-13 13:33:34 +0300

    ads

--- Client/components/cms/CMSDashboard.tsx
+++ Client/components/cms/CMSDashboard.tsx
@@ -1646,16 +1646,7 @@ const SchoolTeamTab: React.FC = () => {
                       {formatDate(image.modified)}
                     </p>
                     
-                    <div className="flex justify-between items-center pt-2 border-t">
-                      <button
-                        onClick={() => navigator.clipboard.writeText(image.url)}
-                        className="text-blue-600 hover:text-blue-800 p-1"
-                        title={t.cms.mediaManager.copyUrl}
-                      >
-                        <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" />
-                        </svg>
-                      </button>
+                    <div className="flex justify-end items-center pt-2 border-t">
                       <button
                         onClick={() => handleDeleteImage(image.filename)}
                         className="text-red-600 hover:text-red-800 p-1"
